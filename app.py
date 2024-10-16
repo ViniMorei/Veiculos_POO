5f005359b4df99eba715db584378dc5073a6d364
ViniMorei <vinicius.moreira.ara@gmail.com>
2024-10-16 15:51:25 -0400

    Populando tabela com txt

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+def ler_dados():
+    with open('veiculos.txt', 'r') as arquivo:
+        linhas = arquivo.readlines()
+
+    dados = []
+    for linha in linhas:
+        id, nome, ano, diaria, tipo, comb, cc = linha.strip().split(',')
+        dados.append({
+            'id' : id,
+            'nome' : nome,
+            'ano' : ano,
+            'diaria' : diaria,
+            'tipo' : tipo,
+            'combustivel' : comb,
+            'cilindradas' : cc
+        })
+    
+    return dados
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,7 +39,8 @@ def alugar_motos():
 
 @app.route('/listar_veiculos')
 def listar_veiculos():
-    return render_template('veiculos.html')
+    dados = ler_dados()
+    return render_template('veiculos.html', dados=dados)
 
 
 if __name__ == '__main__':
--- build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "veiculoPOO.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "veiculoPOO.zip" -Force
