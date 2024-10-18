9dbf9dc8b30349b3ec3036232ef8f96c99d0b794
ViniMorei <vinicius.moreira.ara@gmail.com>
2024-10-18 17:15:37 -0400

    Definindo representação textual dos modelos

--- api/models/carro_model.py
+++ api/models/carro_model.py
@@ -12,3 +12,7 @@ class Carro(Base):
     ano: Mapped[int] = mapped_column(Integer, nullable=False)
     diaria: Mapped[float] = mapped_column(Float, nullable=False)
     combustivel: Mapped[str] = mapped_column(String(50), nullable=False)
+
+
+    def __repr__(self):
+        return f'<Carro(id={self.id}, nome="{self.nome}", ano={self.ano}, diaria={self.diaria}, combustivel="{self.combustivel}")>'
--- api/models/motocicleta_model.py
+++ api/models/motocicleta_model.py
@@ -12,3 +12,7 @@ class Motocicleta(Base):
     ano: Mapped[int] = mapped_column(Integer, nullable=False)
     diaria: Mapped[float] = mapped_column(Float, nullable=False)
     cilindradas: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Moto(id={self.id}, nome="{self.nome}", ano={self.ano}, diaria={self.diaria}, cilindradas="{self.cilindradas}")>'
